ac79ccdfa4edc6c6962587fee9687110e529536a
zeratul33 <zeratul_he@hotmail.com>
2021-06-03 20:20:35 +0800

    master

--- spider.py
+++ spider.py
@@ -6,13 +6,19 @@ import json
 import re
 
 url = "http://74.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112406112931804091546_1622624859371&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1622624859547"
+header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
 response = urllib.request.urlopen(url)
 #print(response)
-comments = requests.get(url)
-pat = '"diff":\[\{.*?\}\]'
+comments = requests.get(url,headers = header).text
+
+pat = 'jQuery(.*?)\('
 #comments = comments.encoding('utf-8')
 #print(comments.text)
-data = re.compile(pat,re.S).findall(comments.text)
-data = json.loads(data)
-print(data)
+re_sub = re.sub('jQuery(.*?)\(',' ',comments)
+re_sub2 = re.sub('\)\;',' ',re_sub)
+#data = re.compile(pat).findall(comments)
+#datas = data[0].split(',')
+#data = re.search(data,url)
+json_sub = json.loads(re_sub2)
+print(json_sub['data']['diff'][1]['f1'])
 
